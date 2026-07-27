84d32d1878930882d8ae22808c925ab917acfe08
Sander van Dijk <sgvandijk@protonmail.com>
2026-07-27 23:23:41 +0200

    Rebase from 'release/lyrical/v4l2_camera'

--- include/v4l2_camera/visibility_control.h
+++ include/v4l2_camera/visibility_control.h
@@ -19,31 +19,31 @@
 //     https://gcc.gnu.org/wiki/Visibility
 
 #if defined _WIN32 || defined __CYGWIN__
-  #ifdef __GNUC__
-    #define ROS2_V4L2_CAMERA_EXPORT __attribute__ ((dllexport))
-    #define ROS2_V4L2_CAMERA_IMPORT __attribute__ ((dllimport))
-  #else
-    #define ROS2_V4L2_CAMERA_EXPORT __declspec(dllexport)
-    #define ROS2_V4L2_CAMERA_IMPORT __declspec(dllimport)
-  #endif
-  #ifdef ROS2_V4L2_CAMERA_BUILDING_LIBRARY
-    #define ROS2_V4L2_CAMERA_PUBLIC ROS2_V4L2_CAMERA_EXPORT
-  #else
-    #define ROS2_V4L2_CAMERA_PUBLIC ROS2_V4L2_CAMERA_IMPORT
-  #endif
-  #define ROS2_V4L2_CAMERA_PUBLIC_TYPE ROS2_V4L2_CAMERA_PUBLIC
-  #define ROS2_V4L2_CAMERA_LOCAL
+#ifdef __GNUC__
+#define ROS2_V4L2_CAMERA_EXPORT __attribute__((dllexport))
+#define ROS2_V4L2_CAMERA_IMPORT __attribute__((dllimport))
 #else
-  #define ROS2_V4L2_CAMERA_EXPORT __attribute__ ((visibility("default")))
-  #define ROS2_V4L2_CAMERA_IMPORT
-  #if __GNUC__ >= 4
-    #define ROS2_V4L2_CAMERA_PUBLIC __attribute__ ((visibility("default")))
-    #define ROS2_V4L2_CAMERA_LOCAL  __attribute__ ((visibility("hidden")))
-  #else
-    #define ROS2_V4L2_CAMERA_PUBLIC
-    #define ROS2_V4L2_CAMERA_LOCAL
-  #endif
-  #define ROS2_V4L2_CAMERA_PUBLIC_TYPE
+#define ROS2_V4L2_CAMERA_EXPORT __declspec(dllexport)
+#define ROS2_V4L2_CAMERA_IMPORT __declspec(dllimport)
+#endif
+#ifdef ROS2_V4L2_CAMERA_BUILDING_LIBRARY
+#define ROS2_V4L2_CAMERA_PUBLIC ROS2_V4L2_CAMERA_EXPORT
+#else
+#define ROS2_V4L2_CAMERA_PUBLIC ROS2_V4L2_CAMERA_IMPORT
+#endif
+#define ROS2_V4L2_CAMERA_PUBLIC_TYPE ROS2_V4L2_CAMERA_PUBLIC
+#define ROS2_V4L2_CAMERA_LOCAL
+#else
+#define ROS2_V4L2_CAMERA_EXPORT __attribute__((visibility("default")))
+#define ROS2_V4L2_CAMERA_IMPORT
+#if __GNUC__ >= 4
+#define ROS2_V4L2_CAMERA_PUBLIC __attribute__((visibility("default")))
+#define ROS2_V4L2_CAMERA_LOCAL __attribute__((visibility("hidden")))
+#else
+#define ROS2_V4L2_CAMERA_PUBLIC
+#define ROS2_V4L2_CAMERA_LOCAL
+#endif
+#define ROS2_V4L2_CAMERA_PUBLIC_TYPE
 #endif
 
 #endif  // V4L2_CAMERA__VISIBILITY_CONTROL_H_
